bb6348e604553455eca239008ce3f099573b795a
nangseakheng <seakhengclever@gmail.com>
2023-09-03 02:37:24 +0700

    bug

--- server/api/users.delete.ts
+++ server/api/users.delete.ts
@@ -1,4 +1,6 @@
-import { PrismaClient } from "prisma/prisma-client/index.js";
+// import { PrismaClient } from "prisma/prisma-client/index.js";
+import { PrismaClient } from '@prisma/client'
+
 const prisma = new PrismaClient();
 
 export default defineEventHandler(async (event) => {
--- server/api/users.put.ts
+++ server/api/users.put.ts
@@ -1,4 +1,5 @@
-import { PrismaClient } from "prisma/prisma-client/index.js";
+// import { PrismaClient } from "prisma/prisma-client/index.js";
+import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient();
 
 export default defineEventHandler(async (event) => {
